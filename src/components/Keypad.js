0f3e9b6e20cc73c7e19ad89a827c2bf902364930
Oliver Spring <oliver9589@gmail.com>
2017-09-18 04:34:30 +0000

    automatically backed up by learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -2,14 +2,13 @@ import React from 'react';
 
 class Keypad extends React.Component {
   render() {
+
     handleInput = () => {
       console.log('Entering password...');
     }
 
     return (
-      <input> type="password"
-      onKeyUp={this.handleInput}
-      </input>
+      <input>type="password" onKeyUp={this.handleInput}</input>
     );
   }
 }
