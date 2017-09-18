bfe9e0ceb5d22b3a34b845cbe6c6ed658e574189
Oliver Spring <oliver9589@gmail.com>
2017-09-18 04:35:37 +0000

    automatically backed up by learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+  handleInput = () => {
+    console.log('Entering password...');
+  }
+
+  render() {
+    return (
+      <input>type="password" onKeyUp={this.handleInput}</input>
+    );
+  }
+}
+
+export default EyesOnMe;
